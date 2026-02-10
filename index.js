cb0cb8c267f0a5d6cc9ece056c100aef8706554f
jfooliv-afk <jfooliv@gmail.com>
2026-02-10 16:19:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import cron from "node-cron";
 import dotenv from "dotenv";
@@ -7,8 +8,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Rota principal
 app.get("/", (req, res) => {
   res.send("App rodando no Render!");
+});
+
+// Nova rota de gastos
+app.get("/gastos", (req, res) => {
+  res.json([
+    { descricao: "Mercado", valor: 120, categoria: "alimentação" },
+    { descricao: "Uber", valor: 35, categoria: "transporte" },
+    { descricao: "Aluguel", valor: 950, categoria: "moradia" }
+  ]);
 });
 
 // Cron de teste: a cada minuto
@@ -16,6 +27,7 @@ cron.schedule("*/1 * * * *", async () => {
   console.log("Cron rodou:", new Date().toLocaleTimeString());
 });
 
+// Inicialização do servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
